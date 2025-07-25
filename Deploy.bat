6c7b7b23ae590f17dcec3e7cf2214a272a532a84
YCH <943696882@qq.com>
2025-07-25 13:49:27 +0800

    onekey deploy

--- Deploy.bat
+++ Deploy.bat
@@ -1,5 +1,4 @@
 git add .
 git commit -m "onekey deploy"
 git push
-curl --header "Content-Type: application/json" --request POST --data "{}" http://flow-openapi.aliyun.com/pipeline/webhook/12ukphIqqkEXcodlKdFc
 
