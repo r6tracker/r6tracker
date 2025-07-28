477d07585226a9a8024b40896e12c30b9d34f2d3
YCH <943696882@qq.com>
2025-07-28 13:45:27 +0800

    fix some

--- src/consts.ts
+++ src/consts.ts
@@ -1,14 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
-import type { Multilingual } from '@/i18n';
+import setting from '../Setting.json';
 
-export const SITE_URL="https://ych.show";
-
-export const SITE_TITLE: string | Multilingual = {
-    en:'ych.show',
-    zh: 'ych.show',
-};
-export const SITE_DESCRIPTION:string|Multilingual ={
-    en: 'this is my personal blog, recording my learning and life moments.',
-    zh: '这是我的个人博客，记录我的学习和生活点滴。',
-};
+export const SITE_URL = setting.SITE_URL;
+export const SITE_TITLE = setting.SITE_TITLE;
+export const SITE_DESCRIPTION = setting.SITE_DESCRIPTION;
--- src/locales.ts
+++ src/locales.ts
@@ -2,28 +2,16 @@
 // Set the languages you want to support on your site.
 // https://astro-i18n-starter.pages.dev/setup/
 
-export const DEFAULT_LOCALE_SETTING: string = "en";
+import setting from '../Setting.json';
+
+export const DEFAULT_LOCALE_SETTING: string = setting.DEFAULT_LOCALE_SETTING;
 
 interface LocaleSetting {
-	[key: Lowercase<string>]: {
-		label: string;
-		lang?: string;
-		dir?: "rtl" | "ltr";
-	};
+  [key: Lowercase<string>]: {
+	label: string;
+	lang?: string;
+	dir?: "rtl" | "ltr";
+  };
 } // refer: https://starlight.astro.build/reference/configuration/#locales
 
-export const LOCALES_SETTING: LocaleSetting = {
-
-	en: {
-		label: "English",
-		lang: "en-US",
-	},
-	it: {
-		label: "Italiano",
-		lang: "it-IT",
-	},
-	de: {
-		label: "Deutsch",
-		lang: "de-DE",
-	},
-};
+export const LOCALES_SETTING: LocaleSetting = setting.LOCALES_SETTING;
